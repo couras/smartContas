8cb81cf17b19df52ebe9e2d9434b288f6ae8b93f
Denis Couras <deniscouras@gmail.com>
2018-08-09 22:33:21 -0300

    iniciando aplicação

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController } from 'ionic-angular/umd';
+
+import { ToastrService } from 'ngx-toastr';
+import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
+
+import { DataService } from '../../app/Services/DataService';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+export class HomePage implements OnInit {
+
+  public message: string;
+    public values: any[];
+
+  constructor(public navCtrl: NavController, 
+    private _dataService: DataService,
+    private _toasterService: ToastrService,
+    private _slimLoadingBarService: SlimLoadingBarService) {
 
   }
+
+  ngOnInit() {
+    this._slimLoadingBarService.start();
+
+    this._dataService
+        .getAll<any[]>()
+        .subscribe((data: any[]) => this.values = data,
+        error => () => {
+          this._toasterService.error('everything is broken', 'Major Error', {
+            timeOut: 3000
+          });
+        },
+        () => {
+          this._toasterService.success('OK', 'Tudo OK'); 
+            this._slimLoadingBarService.complete();
+        });
+}
 
 }
